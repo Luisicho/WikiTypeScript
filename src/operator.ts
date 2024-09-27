338302fc9e54a306cbdfbd5129f2bf068257ae03
Luisicho <luism18.1999@gmail.com>
2024-09-27 13:12:14 -0700

    Ejemplo de operador

---
+++ src/operator.ts
@@ -0,0 +1,11 @@
+const hob = ["Futbol", "Video Juegos"];
+const extraHob = ["Brincar"];
+
+extraHob.push(...hob); //Agrego hob a extraHob
+
+const num = {
+    valor: 2,
+    signo: "+"
+}
+
+const copiNum = {...num} //Copia de el objeto
