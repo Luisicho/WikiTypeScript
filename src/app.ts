5b8e7d3a43fddbf21dab0db6e23d6ff8cdcfd147
Luisicho <luism18.1999@gmail.com>
2024-10-02 09:57:25 -0700

    Metodos Staticos

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,45 @@
 class Department {
   // name: string;
-  protected employees: string [] = [];
+  protected employees: string[] = [];
 
-  get Employees(){
+  get Employees() {
     return this.employees;
   }
 
-  set Employees(E:string[]){
+  set Employees(E: string[]) {
     this.Employees = E;
   }
 
-  constructor(private readonly id: string,public name: string){}
-  describe(){
-    console.log('Departamento: '+ this.name)
+  static createEmployee(name: string) {
+    return { name: name };
   }
-  addEmployee(employe: string){
+
+  constructor(private readonly id: string, public name: string) {}
+  describe() {
+    console.log("Departamento: " + this.name);
+  }
+  addEmployee(employe: string) {
     this.employees.push(employe);
   }
-  printEmployeeInformation(){
+  printEmployeeInformation() {
     console.log(this.employees.length);
     console.log(this.employees);
   }
 }
 
-class ITDepartment extends Department{
-  constructor(id: string, private admins: string[]){
-    super(id,"IT") //Constructor de Padre
-    console.log(this.employees)
+class ITDepartment extends Department {
+  constructor(id: string, private admins: string[]) {
+    super(id, "IT"); //Constructor de Padre
+    console.log(this.employees);
   }
 }
 
-const contaduria = new Department("C1","Contaduria");
+const contaduria = new Department("C1", "Contaduria");
+
+const empleado = Department.createEmployee("luis");
 
 contaduria.addEmployee("Luis");
 contaduria.addEmployee("Miguel");
 
 contaduria.describe();
 contaduria.printEmployeeInformation();
-
-
