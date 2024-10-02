c29476d125c8d475cc5a04c818de54e0b61341d1
Luisicho <luism18.1999@gmail.com>
2024-10-02 12:32:08 -0700

    Interface Implements

---
+++ src/proyect/interface.ts
@@ -0,0 +1,27 @@
+interface Saludo {
+    name: string;
+
+    agree(frase: string): void;
+}
+
+let user1: Saludo;
+
+class Persona implements Saludo{
+    name:string;
+    age = 30;
+
+    constructor(n: string){
+        this.name = n;
+    }
+
+    agree(frase: string): void {
+        console.log(frase);
+    }
+}
+
+user1 = {
+    name: "Luis",
+    agree(frase:string){
+        console.log(frase);
+    }
+}
