e718546e117e02e4b99177da69cf238538a9a2b6
Luisicho <luism18.1999@gmail.com>
2024-10-02 12:43:01 -0700

    Interface and Implements format

--- src/proyect/interface.ts
+++ src/proyect/interface.ts
@@ -1,27 +1,24 @@
 interface Saludo {
-    name: string;
-
-    agree(frase: string): void;
+  name: string;
+  agree(frase: string): void;
 }
 
 let user1: Saludo;
 
-class Persona implements Saludo{
-    name:string;
-    age = 30;
-
-    constructor(n: string){
-        this.name = n;
-    }
-
-    agree(frase: string): void {
-        console.log(frase);
-    }
+class Persona implements Saludo {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
+  agree(frase: string): void {
+    console.log(frase);
+  }
 }
 
 user1 = {
-    name: "Luis",
-    agree(frase:string){
-        console.log(frase);
-    }
-}
+  name: "Luis",
+  agree(frase: string) {
+    console.log(frase);
+  },
+};
