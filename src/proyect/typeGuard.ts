cfa7c318ecc25f80ca2d8e7b2af3a01bb9603d11
Luisicho <luism18.1999@gmail.com>
2024-11-14 12:02:15 -0700

    Sobrecarga F

--- src/proyect/typeGuard.ts
+++ src/proyect/typeGuard.ts
@@ -1,5 +1,7 @@
 type Combinable2 = string | number;
 
+function sumatoria3(a:number, b:number): number;
+function sumatoria3(a:string, b:string): string;
 function sumatoria3(a:Combinable2, b:Combinable2){
     if(typeof a === 'string' || typeof b === 'string'){
         return a.toString() + b.toString();
