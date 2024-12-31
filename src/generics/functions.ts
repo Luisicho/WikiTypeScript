bfed029ff95e41de96aeee670658a1714d999e31
Luisicho <luism18.1999@gmail.com>
2024-12-31 10:43:04 -0700

    Funciones

---
+++ src/generics/functions.ts
@@ -0,0 +1,7 @@
+// function mezclar<T, U>(objA:T, objB:U): T & U{
+//     return Object.assign(objA,objB);
+// }
+
+// const mergeObj = mezclar<{name:string},{edad:number}>({name:'Luis'},{edad:25});
+// console.log(mergeObj.name);
+
