47c47b717b58ff46d3a01bc4b453a1de61638922
Luisicho <luism18.1999@gmail.com>
2025-01-03 12:17:11 -0700

    Funciones Genericas Mas

--- src/generics/functions.ts
+++ src/generics/functions.ts
@@ -5,3 +5,16 @@
 // const mergeObj = mezclar<{name:string},{edad:number}>({name:'Luis'},{edad:25});
 // console.log(mergeObj.name);
 
+interface Lengthy {
+    length: number;
+}
+
+function contAndDescribe<T extends Lengthy>(element:T): [T , string]{
+    let descriptionText = "No valor";
+    if(element.length > 0){
+        descriptionText = "tiene " + element.length + " elementos";
+    }
+    return [element, descriptionText];
+}
+
+console.log(contAndDescribe("Hola Amiga"));
